11e23e33dd4691e5efe1022137fb73665170cc8b
hirokitkhs <hirokinzitou@googlemail.com>
2018-11-13 17:47:37 +0900

    upload

--- 1-3/read_file1.py
+++
@@ -1,9 +0,0 @@
-## read text file by "with open"
-## usage python read_file1.py
-
-input="../1-1/reference/s288c.gff"
-
-with open(input, 'r') as f:
-    for line in f:
-        line=line.rstrip()
-        print(line)
